b9f17b6e6616aef293c0facefb83c2502ab8f066
Vimal Mohan Jain <31728709+i033540@users.noreply.github.com>
2022-11-04 19:30:27 +0530

    Points with constant

--- Week1/points.c
+++ Week1/points.c
@@ -2,15 +2,16 @@
 
 int main(void)
 {
+    const int MINE = 2;
     int points;
     printf("How many points did you lose? ");
     scanf("%i", &points);
 
-    if (points < 2)
+    if (points < MINE)
     {
         printf("You lost fewer points than me.\n");
     } 
-    if (points > 2)
+    if (points > MINE)
     {
         printf("You lost more points than me.\n");
     }

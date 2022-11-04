d85bc5cb50319d4e4a19b044f40503c4912fa42e
Vimal Mohan Jain <31728709+i033540@users.noreply.github.com>
2022-11-04 19:20:40 +0530

    Week 1 - learning to program in C

---
+++ Week1/HelloWorld.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+   //string sName = get_string("What's your name? ");
+   printf("What's your name? ")
+   scanf("%s", &sName);
+   printf("~~~~~~~~~~~~~~\nHello , Vimal!\n~~~~~~~~~~~~~~\n");
+}
